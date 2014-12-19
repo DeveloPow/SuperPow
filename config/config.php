cfcbe42a4246f8d2ff27b4cc64143c474aad1c07
Franco Ocaranza <franco@insignia4u.com>
2014-12-19 17:10:38 -0300

    Creation config.php

---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?
+
+/*  CONFIGURACIÓN GLOBAL
+	--------------------
+
+	Posibles valores:
+
+	a) - "local"	(En caso de estar trabajando sobre el servidor local)
+	b) - "remoto"	(Si se está usando un servidor remoto)
+	C) - "otro"		(Cuando se está usando un tercer servidor para testing u otro uso)
+
+*/
+
+$pow['global']['location'] = "local";
+
+
+/*  CONFIGURACIÓN DE RUTAS
+	----------------------
+
+	En esta sección se configura globalmente las rutas de acceso al árbol del CMS.
+
+*/
+
+// Nombre del servidor (ej: superpow.net)
+$pow['global']['domain'] = $_SERVER['SERVER_NAME'];
+
+
+
+
+
+
+
+?>
